b3b17fce1a8984cf2a041862294f73e86ad8cb97
Florin Vlaicu <19238716+fvlaicu@users.noreply.github.com>
2026-01-12 20:33:18 +0200

    ensure that the basedevice is the tv as a fallback

--- xbmc/peripherals/devices/PeripheralCecAdapter.cpp
+++ xbmc/peripherals/devices/PeripheralCecAdapter.cpp
@@ -354,6 +354,12 @@ bool CPeripheralCecAdapter::OpenConnection(void)
       iTries++;
     }
 
+    if (config.baseDevice == CECDEVICE_UNKNOWN)
+    {
+      CLog::Log(LOGWARNING, "{} - CEC adapter failed to detect base device, defaulting to TV.", __FUNCTION__);
+      config.baseDevice = CECDEVICE_TV;
+    }
+
     if (!m_bStop)
     {
       // update the local configuration
